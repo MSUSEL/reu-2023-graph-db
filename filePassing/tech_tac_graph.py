7adbfe57ff4ecd2a0abd9c788540a64d79eee69d
Aurora Duskin <31018800+A-Duskin@users.noreply.github.com>
2023-06-23 11:17:46 -0600

    Added in comments

--- filePassing/tech_tac_graph.py
+++ filePassing/tech_tac_graph.py
@@ -1,55 +1,40 @@
+# imports 
 from arango import ArangoClient
 import networkx as nx
-#import matplotlib.pyplot as plt
 from pyvis import network as net
 from IPython.core.display import display, HTML
 import webbrowser
 import os
 
-# client = ArangoClient()
-# db = client.db('BRON', username='root', password='changeme')
-
-
+# method to create web browser graph
 def make_graph(db, cursor):
+    # setting up the graph
     TacTac = db.collection('TacticTactic')
     graph = nx.DiGraph()
     g = net.Network(height='100vh', width='100%', notebook=True)
 
-    # query = 'let iterable = ( ' \
-    #         + 'for item in cve ' \
-    #         + 'filter item.original_id in [\'CVE-2019-15243\',\'CVE-2020-3177\',\'CVE-2020-1472\'] ' \
-    #         + 'for e, v, p in 1..5 inbound ' \
-    #         + 'item CweCve, CapecCwe, TechniqueCapec ' \
-    #         + 'filter length(p.edges) > 2 ' \
-    #         + 'return distinct Last(p.vertices) ' \
-    #         + ') ' \
-    #         + 'for item in iterable ' \
-    #         + 'for e, v, p in 1..2 inbound ' \
-    #         + 'item TacticTechnique ' \
-    #         + 'return { From: v._from, To: v._to }'
-    
-    #cursor = db.aql.execute(query)
-
+    # list for easy checking later
     tactech_from_list = []
 
+    # takes the ._from ._to edge from the cursor query
     for item in cursor:
         key, value = item.items()
+        # adding nodes and edge into networkx graph
         graph.add_nodes_from([key[1], value[1]])
         graph.add_edge(key[1], value[1])
+        # adding tactic into list for easy checking
         tactech_from_list.append(key[1])
 
+    # this is checking the BRON database for tactic to tactic 
+    # paths that need to be placed into the networkx graph
     for tt in TacTac:
         if tt['_from'] in tactech_from_list and tt['_to'] in tactech_from_list:
             graph.add_edge(tt['_from'], tt['_to'])
 
-    #nx.draw_networkx(graph)
-    #plt.show()
+    # translates networkx graph into PyViz graph
     g.from_nx(graph)
+    # this is a list of sliders that can interact with the graph, adds things like wind and gravity
     #g.show_buttons(filter_=['physics'])
     g.show('custom.html')
+    # open the custom PyViz graph in the default web browser
     webbrowser.open('file://' + os.path.abspath(os.getcwd()) + '/custom.html')
-
-# def main():
-#     makegraph()
-
-# main()
