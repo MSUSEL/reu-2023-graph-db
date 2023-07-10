09b765eac824e32c7e7d0c4348d740589731f23a
na245 <noahjgysub@gmail.com>
2023-07-10 16:32:30 -0600

    added new methods

--- filePassing/tech_tac_graph.py
+++ filePassing/tech_tac_graph.py
@@ -3,6 +3,7 @@ import networkx as nx
 from pyvis import network as net
 import webbrowser
 import os
+import json
 
 # method to create web browser graph
 def make_graph(db, cursor):
@@ -12,9 +13,10 @@ def make_graph(db, cursor):
     # setting up the graph
     tac_tac = db.collection('TacticTactic')
     graph = nx.Graph()
-    g = net.Network(height='100vh', width='100%', notebook=True)
+    g1 = net.Network(height='100vh', width='100%', notebook=True)
+    g2 = net.Network(height='100vh', width='100%', notebook=True)
 
-    # NOTE: Temp
+    # graph that contains only the paths between tactics
     tac_graph = nx.DiGraph()
 
     # list for easy checking later
@@ -29,79 +31,31 @@ def make_graph(db, cursor):
         # adding tactic into list for easy checking
         tactech_from_list.append(key[1])
 
-    start_nodes = []
 
     # this is checking the BRON database for tactic to tactic 
     # paths that need to be placed into the networkx graph
     for tt in tac_tac:
-        # if tt['_from'] in tactech_from_list and tt['_to'] in tactech_from_list:
-        #     graph.add_edge(tt['_from'], tt['_to'])
-        #     tac_graph.add_edge(tt['_from'], tt['_to'])
-        if tt['_to'] in tactech_from_list:
-            if tt['_from'] in tactech_from_list:
-                graph.add_edge(tt['_from'], tt['_to'])
-                tac_graph.add_edge(tt['_from'], tt['_to'])
-            else:
-                start_nodes.append(tt['_to'])
-    # print(start_nodes)
+        if tt['_from'] in tactech_from_list and tt['_to'] in tactech_from_list:
+            graph.add_edge(tt['_from'], tt['_to'])
+            tac_graph.add_edge(tt['_from'], tt['_to'])
 
-    # start_node = ''
-    # for node in tac_graph:
-    #     if node in start_nodes:
-    #         start_node = node
-    #         break
+    show_prioritize(graph) # runs algorithm that finds the prioritize paths
 
-    show_prioritize(graph)
-    #show_prioritize(tac_graph)
-
-    net_flow_graph = nx.DiGraph()
-    # net_flow_graph.add_node('source')
-    # net_flow_graph.add_node('sink')
-    SRC = 'source[s]'
-    SINK = 'sink[t]'
-
-    nodes = [[SRC]]
-    for n in tac_graph.__iter__():
-        tech = []
-        neighbors = graph.neighbors(n)
-        for node in neighbors:
-            if 'technique' in node:
-                tech.append(n.split('/')[-1] + '/' + node.split('/')[-1])
-        nodes.append(tech)
-
-    nodes.append([SINK])
-
-    # net_flow_graph.add_node(SINK)
-    for i in range(len(nodes)):
-        for j in range(len(nodes[i])):
-            net_flow_graph.add_node(nodes[i][j])
-            #net_flow_graph.add_edge()
-
-    for i in range(len(nodes)-1):
-        for j in range(len(nodes[i])):
-            if nodes[i][j] == 'source[s]':
-                capa = len(nodes[i+1])
-            else:
-                capa = len(nodes[i])
-            for k in range(len(nodes[i+1])):
-                net_flow_graph.add_edge(nodes[i][j], nodes[i+1][k], capacity=capa, title=capa)
-    print('len:', net_flow_graph.__len__())
-    #net_flow_graph.add_node(nodes[0])
-
-    # pos = nx.spring_layout(net_flow_graph)
-    # labels = nx.get_edge_attributes(net_flow_graph, 'capacity')
-    # nx.draw(net_flow_graph, pos)
-    # nx.draw_networkx_edge_labels(net_flow_graph, pos, edge_labels=labels)
-
+    net_flow_graph = make_net_flow_graph(graph, tac_graph)
 
     # translates networkx graph into PyViz graph
-    g.from_nx(net_flow_graph)
+    g1.from_nx(graph)
+    g1.force_atlas_2based() # changing the layout of the graph
+    g1.show('graph.html')
 
-    #TODO: turn off the physics without clicking button
-    g.show_buttons(filter_=['physics'])
-    g.show('graph.html')
+    # translates network flow graph into PyViz graph
+    g2.from_nx(net_flow_graph)
+    g2.force_atlas_2based() # changing the layout of the graph
+    g2.show('network_flow.html')
+
     # open the custom PyViz graph in the default web browser
     webbrowser.open('file://' + os.path.abspath(os.getcwd()) + '/graph.html')
+    webbrowser.open('file://' + os.path.abspath(os.getcwd()) + '/network_flow.html')
 
 
 # method to sort the individual priority lists
@@ -109,6 +63,7 @@ def sort_list(a_list):
     return sorted(a_list, key=lambda tup: tup[1], reverse=False)  # lambda arguments : expression
 
 
+# finds priority of tactic
 def show_prioritize(graph):
     # priority of tactic
     high = []
@@ -151,3 +106,46 @@ def show_prioritize(graph):
         graph.add_node(low[0][0], color='red')
     else:
         pass
+
+
+# creates a network flow graph
+def make_net_flow_graph(graph, tac_graph):
+    net_flow_graph= nx.DiGraph()
+    SRC = 'source[s]' # starting point of the graph
+    SINK = 'sink[t]' # ending point of the graph
+
+    # prepares unique nodes for the graph
+    nodes = [[SRC]] # starting node
+    for n in tac_graph.__iter__(): # gets tactic in order of path
+        tech = []
+        neighbors = graph.neighbors(n)
+        for node in neighbors: # loops to get all techniques
+            if 'technique' in node:
+                tech.append(n.split('/')[-1] + '/' + node.split('/')[-1]) # assigns unique name
+        nodes.append(tech)
+    nodes.append([SINK]) # ending node
+
+    # adds nodes and edges to the network flow graph
+    for i in range(len(nodes)-1):
+        for j in range(len(nodes[i])):
+            net_flow_graph.add_node(nodes[i][j])
+
+            # checks what capacity to set
+            if nodes[i][j] == 'source[s]':
+                capa = len(nodes[i+1])
+            else:
+                capa = len(nodes[i])
+            for k in range(len(nodes[i+1])):
+                net_flow_graph.add_edge(nodes[i][j], nodes[i+1][k], capacity=capa, title=capa)
+    
+    return net_flow_graph
+
+# creates a json file that contains all simple paths of network flow graph
+def create_simple_paths_json(net_flow_graph, src, sink):
+    simple_paths = [] # lists that will hold the json objects
+    for path in nx.all_simple_paths(net_flow_graph, src, sink):
+        simple_paths.append({'path': path})
+    
+    # creates and adds the json objects to the file
+    with open('net_flow_graph.json', 'w') as out_file:
+        json.dump(simple_paths, out_file, indent=2)
