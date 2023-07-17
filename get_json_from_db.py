9d6cfe42de48e4fccd4d98e6f1526b27b8fba009
na245 <noahjgysub@gmail.com>
2023-07-17 15:06:28 -0600

    Fixed a bug

--- get_json_from_db.py
+++ get_json_from_db.py
@@ -1,8 +1,11 @@
 # This script gets data of techniqueCapac edge collection from the BRON guest db
 
-from arango import ArangoClient
+from arango import ArangoClient, DocumentInsertError
 
 def get_tech_capac(db):
+
+    # constant that indicates the collection we're going to use
+    TECH_CAP = 'TechniqueCapec'
     
     # setting guest database and local database
     guest = ArangoClient(hosts='http://bron.alfa.csail.mit.edu:8529')
@@ -11,23 +14,37 @@ def get_tech_capac(db):
     guest_db = guest.db('BRON', username='guest', password='guest')
 
     # opening guest db document for transfer
-    guest_collection = guest_db.collection('TechniqueCapec')
+    guest_collection = guest_db.collection(TECH_CAP)
 
     # open local graph for edge placement
     graph = db.graph('BRONGraph')
 
-    # if the collection does not exist in local, create it
-    # (should by default)
-    if not db.has_collection('TechniqueCapec'):
-        db.create_collection('TechniqueCapec')
+    # if the edge definition exists remove it
+    if graph.has_edge_definition(TECH_CAP):
+        graph.delete_edge_definition(TECH_CAP)
+    
+    # if the collection exists remove it
+    if db.has_collection(TECH_CAP):
+        db.delete_collection(TECH_CAP)
 
-    # wipe the local collection clean
-    TC = db.collection('TechniqueCapec')
-    TC.truncate()
+    # creates new edge definition and collection
+    graph.create_edge_definition(
+        edge_collection = TECH_CAP,
+        from_vertex_collections = ['technique'],
+        to_vertex_collections = ['capec'])
+    
+    # gets the edge collection from the database
+    tc = graph.edge_collection(TECH_CAP)
 
     # grab all documents in guest collection
     documents = guest_collection.all()
-
+    
     # place all edges into the local database
     for document in documents:
-        TC.insert(document)
+        try:
+            edge_data = {'_from': document['_from'], '_to': document['_to']}
+            tc.insert(edge_data)
+
+        # skip inserting since the capec(attack pattern) in data is  ot in capec collection
+        except DocumentInsertError:
+            pass
